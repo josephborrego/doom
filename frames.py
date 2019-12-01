4725d656e769c765763e9bc5ad6e982299ee56e3
Joseph Borrego <borregoj2202@gmail.com>
2019-11-30 21:06:04 -0500

    Add files via upload

--- frames.py
+++ frames.py
@@ -2,17 +2,53 @@ import numpy as np
 from skimage import transform
 import skimage.transform
 from collections import deque
+import cv2
+from PIL import Image
 stack_size = 4
 
+# https://towardsdatascience.com/image-pre-processing-c1aec0be3edf
 def preprocess_frame(frame):
-    # Greyscale frame already done in our vizdoom config
-    #x = np.mean(frame,-1)
-    cropped_frame = frame[30:-10,30:-30] # Crop the screen (remove the roof b/c no info)
-    normalized_frame = cropped_frame/255.0 # Normalize Pixel Values
-    preprocessed_frame = skimage.transform.resize(normalized_frame, (84,84)) # Resize
-    preprocessed_frame = preprocessed_frame.astype(np.float32)
-    return preprocessed_frame
+    src = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY).astype('uint8')
+    #cropped_frame = frame[30:-10,30:-30] 
+    #normalized_frame = cropped_frame/255.0 
+    cropped_frame = src[30:-10, 30:-30]
+    normalized_frame = cropped_frame/255.0
+    #img = cv2.imread(normalized_frame)
+    #preprocessed_frame = skimage.transform.resize(normalized_frame, (84,84)) 
+    width = int(normalized_frame.shape[1] * 60 / 100)
+    height = int(normalized_frame.shape[0] * 60 / 100)
+    dim = (width, height)
+    preprocessed_frame = cv2.resize(normalized_frame, dim, interpolation=cv2.INTER_LINEAR)
+    #preprocessed_frame = preprocessed_frame.astype(np.float32)
+    # remove gaussian noise
+    blur = cv2.GaussianBlur(preprocessed_frame, (5, 5), 0).astype('uint8')
+    # segmentation & morphology
+    # segment - separating background from foreground objects &  more noise removal
+    # converts to gray scale
+    # gray = cv2.cvtColor(blur, cv2.COLOR_RGB2GRAY)
+    # otsus binarization
+    ret, thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    # apply another blur to improve the looks
+    # further noise removal
+    kernel = np.ones((3, 3), np.uint8)
+    opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=2)
+    # sure background area
+    sure_bg = cv2.dilate(opening, kernel, iterations=3)
+    dist_transform = cv2.distanceTransform(opening, cv2.DIST_L2, 5)
+    ret, sure_fg = cv2.threshold(dist_transform, 0.7 * dist_transform.max(), 255, 0)
+    # finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg, sure_fg)
+    # seperate different objects in the image with markers
+    ret, markers = cv2.connectedComponents(sure_fg)
+    #markers = markers + 1
+    #markers[unknown == 255] == 0
+    #markers = cv2.watershed(frame, markers)
+    #cv2.imshow('yo', sure_fg)
+    return sure_fg
+    #return markers
 
+# stack frames is used for the experience replay buffer
 def stack_frames(stacked_frames, state, is_new_episode):
     frame = preprocess_frame(state)
     if is_new_episode:
--- train.py
+++ train.py
@@ -1,42 +1,58 @@
 from vizdoom import *
 from frames import *
-import dqn
+from dqn import *
 from collections import deque
 import numpy as np
 import random
 import skimage
 import itertools as it
+from memory import *
+import cv2
 
-
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 game = DoomGame()
 game.load_config("borrego.cfg")
 game.set_doom_scenario_path("borrego.wad")
-game.set_window_visible(False)
+game.set_window_visible(True)
 game.set_mode(Mode.PLAYER)
-game.set_screen_format(ScreenFormat.GRAY8)
+game.set_screen_format(ScreenFormat.BGR24)
 game.set_screen_resolution(ScreenResolution.RES_640X480)
 game.init()
 
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+TARGET_UPDATE = 10
+
 model_savefile = "./model-doom.pth"
+save_model = True
+load_model = False
+
 stack_size = 4
 num_episodes = 50
+memory_size = 1000
+memory = Memory(memory_size)
 stacked_frames  =  deque([np.zeros((84,84), dtype=np.int) for i in range(stack_size)], maxlen=4)
 n = game.get_available_buttons_size()
 actions = [list(a) for a in it.product([0, 1], repeat=n)]
 
+if load_model:
+    model = torch.load(model_savefile)
+else:
+    model = DQN(len(actions))
+
+#print(actions)
+
 for i in range(num_episodes):
     game.new_episode()
-    #state = game.get_state().screen_buffer
-    state = game.get_state()
-    img = state.screen_buffer
-    state, stacked_frames = stack_frames(stacked_frames, img, True)
+    state = preprocess_frame(game.get_state().screen_buffer)
+    cv2.imshow('yo', state)
+    #img, stacked_frames = stack_frames(stacked_frames, state, True)
 
     while not game.is_episode_finished():
-        #preprocess part
-        #misc = state.game_variables
         action = random.choice(actions)
-        #DQN part, chooses actions ^^
-        print(action)
         reward = game.make_action(action)
 
 game.close()
